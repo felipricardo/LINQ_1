24f5149dc762ef7cb65ad4803a119fe240d52e8a
felipricardo <felipebarbosaok@gmail.com>
2023-09-11 00:32:15 -0300

    up

--- LINQ_1/Form1.cs
+++ LINQ_1/Form1.cs
@@ -12,9 +12,31 @@ namespace LINQ_1
 {
     public partial class Form1 : Form
     {
+        List<string> lista_nomes;
+        List<int> lista_numeros;
+        Dictionary<string, double> lista_produto;
+        Dictionary<string, string> lista_estados;
+        
         public Form1()
         {
             InitializeComponent();
+
+            #region Lista de Nomes
+            lista_nomes = new List<string>();
+            lista_nomes.Add("Julia");
+            lista_nomes.Add("Felipe");
+            #endregion
+
+            #region Lista de Numeros
+            lista_numeros = new List<string>();
+            lista_nomes.Add("Julia");
+            lista_nomes.Add("Felipe");
+            #endregion
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
