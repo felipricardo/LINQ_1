4d99bbe749283785b2bc4d78e0f43921959620b3
felipricardo <felipebarbosaok@gmail.com>
2023-09-10 23:52:31 -0300

    up

--- LINQ_1/Form1.Designer.cs
+++ LINQ_1/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 29;
+            this.lista.Location = new System.Drawing.Point(12, 16);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(736, 584);
+            this.lista.TabIndex = 0;
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsulta.Location = new System.Drawing.Point(754, 16);
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(280, 41);
+            this.txtConsulta.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1046, 644);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox txtConsulta;
     }
 }
 
