e1a4ceab701417b44e75d04c3f998e8db2288304
felipricardo <felipebarbosaok@gmail.com>
2023-09-12 00:17:32 -0300

    up

--- LINQ_1/Form1.Designer.cs
+++ LINQ_1/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Variável de designer necessária.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpar os recursos que estão sendo usados.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,58 +20,128 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.lista = new System.Windows.Forms.ListBox();
             this.txtConsulta = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnOrderBy = new System.Windows.Forms.Button();
+            this.btnGroupBy = new System.Windows.Forms.Button();
+            this.btnAgregacao = new System.Windows.Forms.Button();
+            this.btnOperadoresDeElemento = new System.Windows.Forms.Button();
+            this.btnLINQLambda = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lista
-            // 
-            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lista.FormattingEnabled = true;
-            this.lista.ItemHeight = 29;
-            this.lista.Location = new System.Drawing.Point(12, 16);
-            this.lista.Name = "lista";
-            this.lista.Size = new System.Drawing.Size(736, 584);
-            this.lista.TabIndex = 0;
             // 
             // txtConsulta
             // 
             this.txtConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtConsulta.Location = new System.Drawing.Point(754, 16);
+            this.txtConsulta.Location = new System.Drawing.Point(644, 13);
             this.txtConsulta.Name = "txtConsulta";
-            this.txtConsulta.Size = new System.Drawing.Size(280, 41);
+            this.txtConsulta.Size = new System.Drawing.Size(211, 41);
             this.txtConsulta.TabIndex = 1;
             // 
-            // button1
+            // btnExecutar
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(754, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(280, 80);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "executar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnExecutar.Location = new System.Drawing.Point(644, 61);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(211, 62);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Location = new System.Drawing.Point(644, 129);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(211, 62);
+            this.btnWhere.TabIndex = 3;
+            this.btnWhere.Text = "where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 38;
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(612, 574);
+            this.lista.TabIndex = 4;
+            // 
+            // btnOrderBy
+            // 
+            this.btnOrderBy.Location = new System.Drawing.Point(644, 197);
+            this.btnOrderBy.Name = "btnOrderBy";
+            this.btnOrderBy.Size = new System.Drawing.Size(211, 62);
+            this.btnOrderBy.TabIndex = 5;
+            this.btnOrderBy.Text = "order by";
+            this.btnOrderBy.UseVisualStyleBackColor = true;
+            this.btnOrderBy.Click += new System.EventHandler(this.btnOrderBy_Click);
+            // 
+            // btnGroupBy
+            // 
+            this.btnGroupBy.Location = new System.Drawing.Point(644, 265);
+            this.btnGroupBy.Name = "btnGroupBy";
+            this.btnGroupBy.Size = new System.Drawing.Size(211, 62);
+            this.btnGroupBy.TabIndex = 6;
+            this.btnGroupBy.Text = "group by";
+            this.btnGroupBy.UseVisualStyleBackColor = true;
+            this.btnGroupBy.Click += new System.EventHandler(this.btnGroupBy_Click);
+            // 
+            // btnAgregacao
+            // 
+            this.btnAgregacao.Location = new System.Drawing.Point(644, 333);
+            this.btnAgregacao.Name = "btnAgregacao";
+            this.btnAgregacao.Size = new System.Drawing.Size(211, 62);
+            this.btnAgregacao.TabIndex = 7;
+            this.btnAgregacao.Text = "Operadores de Agregação";
+            this.btnAgregacao.UseVisualStyleBackColor = true;
+            this.btnAgregacao.Click += new System.EventHandler(this.btnAgregacao_Click);
+            // 
+            // btnOperadoresDeElemento
+            // 
+            this.btnOperadoresDeElemento.Location = new System.Drawing.Point(644, 401);
+            this.btnOperadoresDeElemento.Name = "btnOperadoresDeElemento";
+            this.btnOperadoresDeElemento.Size = new System.Drawing.Size(211, 62);
+            this.btnOperadoresDeElemento.TabIndex = 8;
+            this.btnOperadoresDeElemento.Text = "Operadores de Elemento";
+            this.btnOperadoresDeElemento.UseVisualStyleBackColor = true;
+            this.btnOperadoresDeElemento.Click += new System.EventHandler(this.btnOperadoresDeElemento_Click);
+            // 
+            // btnLINQLambda
+            // 
+            this.btnLINQLambda.Location = new System.Drawing.Point(644, 469);
+            this.btnLINQLambda.Name = "btnLINQLambda";
+            this.btnLINQLambda.Size = new System.Drawing.Size(211, 62);
+            this.btnLINQLambda.TabIndex = 9;
+            this.btnLINQLambda.Text = "LINQ Lambda";
+            this.btnLINQLambda.UseVisualStyleBackColor = true;
+            this.btnLINQLambda.Click += new System.EventHandler(this.btnLINQLambda_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1046, 644);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtConsulta);
+            this.ClientSize = new System.Drawing.Size(867, 612);
+            this.Controls.Add(this.btnLINQLambda);
+            this.Controls.Add(this.btnOperadoresDeElemento);
+            this.Controls.Add(this.btnAgregacao);
+            this.Controls.Add(this.btnGroupBy);
+            this.Controls.Add(this.btnOrderBy);
             this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtConsulta);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -79,10 +149,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox lista;
         private System.Windows.Forms.TextBox txtConsulta;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnWhere;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnOrderBy;
+        private System.Windows.Forms.Button btnGroupBy;
+        private System.Windows.Forms.Button btnAgregacao;
+        private System.Windows.Forms.Button btnOperadoresDeElemento;
+        private System.Windows.Forms.Button btnLINQLambda;
     }
 }
 
