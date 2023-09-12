db4965a739f19d19c4a3d10e8e559fbd55d3c159
felipricardo <felipebarbosaok@gmail.com>
2023-09-12 10:43:25 -0300

    up

--- LINQ_1/Form1.Designer.cs
+++ LINQ_1/Form1.Designer.cs
@@ -142,6 +142,7 @@
             this.Controls.Add(this.btnExecutar);
             this.Controls.Add(this.txtConsulta);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
